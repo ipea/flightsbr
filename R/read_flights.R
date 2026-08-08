ef3712fe7c1f5d3192560922dc5320eb5acebaa1
baarthur <baarthur0@outlook.com>
2026-08-08 12:11:27 -0300

    fix: use available file URLs in read_flights() (ipea/flightsbr#52)

--- R/read_flights.R
+++ R/read_flights.R
@@ -9,9 +9,10 @@
 #' description of all variables included in the data is available at \url{https://www.gov.br/anac/pt-br/assuntos/regulados/empresas-aereas/Instrucoes-para-a-elaboracao-e-apresentacao-das-demonstracoes-contabeis/descricao-de-variaveis}.
 #'
 #' @param date Numeric. Date of the data in the format `yyyymm`. Defaults to
-#'             `202001`. To download the data for all months in a year, the user
-#'             can pass a 4-digit year input `yyyy`. The parameter also accepts
-#'             a vector of dates such as `c(202001, 202006, 202012)`.
+#'             the latest available month. To download the data for all months
+#'             in a year, the user can pass a 4-digit year input `yyyy`. The
+#'             parameter also accepts a vector of dates such as
+#'             `c(202001, 202006, 202012)`.
 #' @param type String. Whether the data set should be of the type `basica`
 #'             (flight stage, the default) or `combinada` (On flight origin and
 #'             destination - OFOD).
@@ -37,7 +38,7 @@ read_flights <- function(
   cache = TRUE
 ) {
   ### check inputs
-  type <- match.arg(type)
+  requested_type <- match.arg(type)
   if (!is.logical(showProgress)) {
     stop(paste0("Argument 'showProgress' must be either 'TRUE' or 'FALSE."))
   }
@@ -53,27 +54,30 @@ read_flights <- function(
     return(invisible(NULL))
   }
 
-  # subset files: only required type and dates
-  files <- files[type == ..type]
-
-  if(!is.null(date)) {
-    files <- files[date %in% ..date]
-  }
+  # subset files: only required type
+  files <- files[type == requested_type]
   all_dates <- files$date
-
 
   ### check date input
   if (is.null(date)) {
     date <- max(all_dates)
   }
-
   check_date(date = date, all_dates)
+
+  # expand years to months
+  if (all(nchar(date) == 4L)) {
+    date <- generate_all_months(date)
+  }
+
+  # subset files: only required dates
+  requested_dates <- date
+  files <- files[date %in% requested_dates]
 
   #### Download and read data
   dt_list <- download_flights_data(files$url, showProgress, select, cache)
 
   # check if download failed
-  if (is.null(dt)) {
+  if (is.null(dt_list)) {
     return(invisible(NULL))
   }
 
