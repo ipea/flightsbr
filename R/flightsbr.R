29d9e5cd2d6825cac3da3b3a568e8173036f2f07
rafapereirabr <rafa.pereira.br@gmail.com>
2026-08-20 09:39:51 -0300

    docs

--- R/flightsbr.R
+++ R/flightsbr.R
@@ -1,10 +1,10 @@
 #' flightsbr: Download Flight and Airport Data from Brazil
 #'
 #' Download flight and airport data from Brazil’s Civil Aviation Agency (ANAC)
-#'  <https://www.gov.br/anac/pt-br>. The data includes detailed information on all
-#'  aircrafts, aerodromes, airports, and airport movements registered in ANAC,
-#'   and on every international flight to and from Brazil, as well as domestic
-#'   flights within the country.
+#' <https://www.gov.br/anac/pt-br>. The data includes detailed information on all
+#' aircrafts, aerodromes, airports, and airport movements registered in ANAC,
+#' and on every international flight to and from Brazil, as well as domestic
+#' flights within the country.
 #'
 #' @section Usage:
 #' Please check the vignettes and data documentation on the
